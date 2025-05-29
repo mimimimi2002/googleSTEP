b7afb30ccd95e71309c5f9fbc0ec1b751a8426d3
MIKI <mikimiki2002.ma@gmail.com>
2025-05-30 00:56:29 +0900

    cpp module pure python change name

--- week1/runtime_calculator/anagramfinder_cpp_module.py
+++ week1/runtime_calculator/anagramfinder_cpp_module.py
@@ -1,5 +1,6 @@
 import time
 import anagramfinder
+from importlib.resources import files
 
 test_words = []
 with open("random_words.txt" ,'r') as f:
@@ -11,7 +12,8 @@ runtimes = []
 for word in test_words:
   start = time.time()
   # Create instance from dictionary file
-  finder = anagramfinder.AnagramFinder("words.txt")
+  dict_path = files(anagramfinder).joinpath("words.txt")
+  finder = anagramfinder.AnagramFinder(str(dict_path))
 
   # Find anagrams
   print(finder.find_anagram(word))
