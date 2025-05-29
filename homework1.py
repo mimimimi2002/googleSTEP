86f2ad5474af5e7ca3c0e1d0d95aac48d8e695ee
MIKI <mikimiki2002.ma@gmail.com>
2025-05-29 09:26:39 +0900

    update comment

--- homework1.py
+++ homework1.py
@@ -2,12 +2,16 @@ def get_all_anagrams(sorted_new_dictionary, sorted_word, idx):
   """
   This function returns a list of string of all anagrams of
   the word that has the same sorted word as given sorted_word.
+  Assume that sorted_word is in the idx of sorted_new_dictionary.
 
   Args:
-      sorted_new_dictionary (list): this is a list of the tuple of the new dictionary that holds the sorted word and origianl word.
-                                    this is already sorted by the first element of the tuple.
+      sorted_new_dictionary (list): this is a list of the tuple that holds the sorted word and origianl word.
+                                    this is already sorted by the sorted word.
       sorted_word (string): a sorted word that you want to find all anagrams.
       idx (int): the index of the sorted word in the sorted_new_dictionary.
+  Returns:
+      list: a list of string of all anagrams of the word that has the same sorted word as given sorted_word.
+      If it cannot find the sorted_word from the sorted_new_dictionary, they will return an empty list.
   """
 
   left = idx - 1
@@ -29,13 +33,16 @@ def get_all_anagrams(sorted_new_dictionary, sorted_word, idx):
 def binary_serach(sorted_word, sorted_new_dictionary):
   """
   This function does binary search on the sorted word in new dictionary.
-  This function searches the sorted word on the first element of the tuple in new dictioanry.
-  Returns the origianl word if its anagram is in the sorted new dictionary.
-  If it cannot find the anagram from the dictionary, they will return empty string.
+  This function searches the sorted word on the first element of the tuple in sorted_new_dictionary
+  Returns a list of string of origianl word if its sorted_word is in the sorted_new_dictionary
+  If it cannot find the sorted_word from the sorted_new_dictionary, they will return an empty list.
   Args:
       sorted_word (string): this is the sorted word that you want to search from sorted_new_dictionary
-      sorted_new_dictionary (list): this is a list of the tuple of the new dictionary that holds the sorted word and origianl word.
+      sorted_new_dictionary (list): this is a list of the tuple that holds the sorted word and origianl word.
                                     this is already sorted by the first element of the tuple.
+  Returns:
+      list: a list of string of original word that has the same sorted word as the given sorted_word.
+            If it cannot find the sorted_word from the sorted_new_dictionary, they will return an empty list.
   """
   left = 0
   right = len(sorted_new_dictionary) - 1
@@ -43,13 +50,10 @@ def binary_serach(sorted_word, sorted_new_dictionary):
 
   # binary serach
   while left <= right:
-
-    # prevent overflowing
     mid = int(left + (right - left) / 2)
     mid_word = sorted_new_dictionary[mid][0]
 
-    # if find the right sorted word, return the pair of the original word
-    # it does not matter which word will return as long as the sorted word matches the word in the new dict
+    # if find the right sorted word, return all anagrams
     if mid_word == sorted_word:
       return get_all_anagrams(sorted_new_dictionary, sorted_word, mid)
     elif mid_word < sorted_word:
@@ -61,18 +65,25 @@ def binary_serach(sorted_word, sorted_new_dictionary):
 
 def find_anagram(tarted_word, sorted_new_dictionary):
   """
-  This function returns the anagram of the tarted_word if it is in dictionary.
+  This function returns a string of anagram of the tarted_word if it is in dictionary.
   Assume that this function is used in different targed_word in the same dictionary.
-  If the tarted_word is empty, returns "word is empty".
-  If the tarted_word does not have anagram that is in this dictionary, returns "Cannot find the anagram of the word in the dictionary".
+  If the target word has mutiple anagrams, returns a list of all anagrams.
+  If the tarted_word is empty, returns 'Word is empty'.
+  If the tarted_word does not have anagram that is in this dictionary, returns 'Cannot find the anagram of the word in the dictionary'.
   Args:
       targeted_word (string): the word that is needed to find its anagram.
       dictionary (a list of string): a list of string that represents a dictionary.
+  Returns:
+      string or list: a string of anagram of the tarted_word if it is in dictionary.
+                      If the target word has mutiple anagrams, returns a list of all anagrams.
+                      If the tarted_word is empty, returns 'Word is empty'.
+                      If the tarted_word does not have anagram that is in this dictionary,
+                      returns 'Cannot find the anagram of the word in the dictionary'.
   """
 
   # if the targeted word is empty
   if tarted_word == "":
-    return "Word is empty"
+    return 'Word is empty'
 
   # binary search
   sorted_word = "".join(sorted(tarted_word))
@@ -92,6 +103,7 @@ if __name__ == "__main__":
     for line in contents:
       dictionary.append(line.strip())
 
+  # create new dictionary with a pair of sorted word and an original word
   new_dictionary = []
   for word in dictionary:
       new_dictionary.append(("".join(sorted(word)), word))
