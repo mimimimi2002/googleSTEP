7f515c6525d739d9b846d9cd0de8e01518f94943
MIKI <mikimiki2002.ma@gmail.com>
2025-06-03 16:08:38 +0900

    hw4 comment finish

--- week2/homework4.py
+++ week2/homework4.py
@@ -8,83 +8,88 @@ from homework1 import HashTable, Item
 #       to implement the data structure yourself!
 
 class Website:
+  # Initialize the Website node
+  # |url|: A string of the accessed URL
+  # |contents|: A string of the contents of the URL
   def __init__(self, url, contents):
-    self.url = url
-    self.contents = contents
-    self.next = None
-    self.prev = None
+    self.url = url         # The accessed URL
+    self.contents = contents  # The contents of the URL
+    self.next = None       # Pointer to the next Website in the linked list
+    self.prev = None       # Pointer to the previous Website in the linked list
 
 class Cache:
     # Initialize the cache.
     # |n|: The size of the cache.
     def __init__(self, n):
-      self.cache_size = n
-      self.website_node_table = HashTable(self.cache_size)
-      self.head = None
-      self.tail = None
-      self.item_count = 0
+      self.cache_size = n  # The size of the cache
+      self.url_website_node_hash_table = HashTable(self.cache_size) # The hash table of the size of cache, take url as key and take Website node as value.
+      self.head = None     # Pointer to the first element in the linked list
+      self.tail = None     # Pointer to the last element in the linked list
+      self.item_count = 0  # The number of elements in the linekd list
 
     # Access a page and update the cache so that it stores the most recently
     # accessed N pages. This needs to be done with mostly O(1).
     # |url|: The accessed URL
     # |contents|: The contents of the URL
     def access_page(self, url, contents):
-        (website_node, success_to_get) = self.website_node_table.get(url)
-        if success_to_get:
-          print("get the same key")
-          print(website_node.url, website_node.contents)
-          # then make the node to the first
 
-          # if it is not the most recent one
-          if website_node.prev != None:
+      # check if the url as key has already been stored in hash table
+      (website_node, success_to_get) = self.url_website_node_hash_table.get(url)
 
-            # if it is the oldest one
-            if website_node.next == None:
-              print("move the tail to the first")
-              self.tail = self.tail.prev
-              website_node.prev.next = None
+      # if already stored in hash table, delete the node from linked list and make the node to the first.
+      # At the same time, we delete the node from hash table and after make it to the first,
+      # put the updated node to the hash table again.
+      if success_to_get:
 
-            else:
-              print("move the middle one to the first")
-              # delete this node from linked list
-              website_node.prev.next = website_node.next
-              website_node.next.prev = website_node.prev
+        # if it is not the most recent one, divide to two senarios, when it is the middle node or tail node
+        if website_node.prev != None:
 
-            # delete this node from hash table
-            self.website_node_table.delete(url)
-
-            # add the node to the fisrt in linked list
-            website_node.next = self.head
-            website_node.prev = None
-            self.head.prev = website_node
-            self.head = website_node
-
-            self.website_node_table.put(url, website_node)
-
-        # add curr website to the linked list
-        else:
-          curr_website = Website(url, contents)
-
-          # if there is nothing yet in linked list
-          if self.item_count == 0:
-            print("add first element")
-            self.head = curr_website
-            self.tail = curr_website
-            self.website_node_table.put(url, curr_website)
-            self.item_count += 1
-          else:
-            print("add next element")
-            curr_website.next = self.head
-            self.head.prev = curr_website
-            self.head = curr_website
-            self.website_node_table.put(url, curr_website)
-            self.item_count += 1
-
-          # after put if it is over the size of hash table, throw away the tail
-          if self.item_count > self.cache_size:
-            print("throw tail")
+          # if it is the oldest one (it is tail), delete the node from linked list
+          if website_node.next == None:
             self.tail = self.tail.prev
-            self.tail.next = None
+            website_node.prev.next = None
+
+          # if it is middle one, delete the node from linked list
+          else:
+            website_node.prev.next = website_node.next
+            website_node.next.prev = website_node.prev
+
+          # delete this node from hash table
+          self.url_website_node_hash_table.delete(url)
+
+          # add the node to the fisrt in linked list
+          website_node.next = self.head
+          website_node.prev = None
+          self.head.prev = website_node
+          self.head = website_node
+
+          # put the updated node to the hash table
+          self.url_website_node_hash_table.put(url, website_node)
+
+      # If this website has not visited in recent n times, add current website to the linked list
+      else:
+        curr_website = Website(url, contents)
+
+        # if there is nothing yet in linked list, put the node to the head
+        if self.item_count == 0:
+          self.head = curr_website
+          self.tail = curr_website
+          self.url_website_node_hash_table.put(url, curr_website)
+          self.item_count += 1
+
+        # if there is already some nodes in the linked list, put the node to the head
+        # and put the new node to hash map.
+        else:
+          curr_website.next = self.head
+          self.head.prev = curr_website
+          self.head = curr_website
+          self.url_website_node_hash_table.put(url, curr_website)
+          self.item_count += 1
+
+        # after put the current website, if it is over the size of hash table, throw away the tail
+        if self.item_count > self.cache_size:
+          self.tail = self.tail.prev
+          self.tail.next = None
 
     # Return the URLs stored in the cache. The URLs are ordered in the order
     # in which the URLs are mostly recently accessed.
@@ -142,7 +147,6 @@ def cache_test():
     cache.access_page("c.com", "CCC")
     assert cache.get_pages() == ["c.com", "a.com", "d.com", "b.com"]
     cache.access_page("a.com", "AAA")
-    print(cache.get_pages())
     assert cache.get_pages() == ["a.com", "c.com", "d.com", "b.com"]
     cache.access_page("a.com", "AAA")
     assert cache.get_pages() == ["a.com", "c.com", "d.com", "b.com"]
