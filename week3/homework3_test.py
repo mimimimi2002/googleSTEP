eabce9081056dc69ef9915a2f0ebae7268a49a1b
MIKI <mikimiki2002.ma@gmail.com>
2025-06-13 01:08:47 +0900

    update test cases

--- week3/homework3_test.py
+++ week3/homework3_test.py
@@ -9,8 +9,8 @@ def run_test():
     test("23.4567") # only float without symbol, integer + float_dot + float_number part in read_number function
 
     # below test plus and minus function, make sure the +, - part is ok
-    # test("-1") # only integer without symbol, first integer in times and divide function, minus part in plust_minus function
-    # test("-23.4567") # only float without symbol, actually not necessary because it is same as above for testing
+    test("-1") # only integer without symbol, first integer in times and divide function, minus part in plust_minus function
+    test("-23.4567") # only float without symbol, actually not necessary because it is same as above for testing
     test("2+3") # plus function, first integer in times and divide function, plus function in first integer and plus function in second integer.
     test("2-3") # minus function
     test("-1+3") # first is minus mix plus and minus function
@@ -29,7 +29,7 @@ def run_test():
     test("4/3*6+4") # mix plus and times and divide function, put plus last
     test("-3-4/3*6+4") # mix plus and times and divide function, binded with plus and minus
     test("-3-4/3*6+4-7*5") # mix plus and several times and divide function
-    # test("-3.1-4.2/3.5*6.2+4.9-7.4*5.1") # mix plus and several times and divide function for float not actually needed but just in case
+    test("-3.1-4.2/3.5*6.2+4.9-7.4*5.1") # mix plus and several times and divide function for float not actually needed but just in case
 
     # above test the calculate_four_operation function
 
