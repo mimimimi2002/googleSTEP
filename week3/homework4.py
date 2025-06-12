89f13cbc817ebcea94652880ff4912ccd83682f2
MIKI <mikimiki2002.ma@gmail.com>
2025-06-13 01:05:48 +0900

    clear comment

--- week3/homework4.py
+++ week3/homework4.py
@@ -84,6 +84,8 @@ def tokenize(line):
       tokens.append(token)
   return tokens
 
+# parse parentheses to get the answer
+# |tokens| : a list of dictionary that represents each toekn
 def parse_parentheses(tokens):
   index = 0
   if tokens[index]['type'] == 'PARENTHESES_LEFT' and tokens[-1]['type'] == 'PARENTHESES_RIGHT':
@@ -93,21 +95,30 @@ def parse_parentheses(tokens):
     print('Invalid syntax')
     exit(1)
 
+# parse factor to get the answer
+# |tokens| : a list of dictionary that represents each toekn
 def parse_factor(tokens):
     index = 0
+
+    # when factor is Literal
     if tokens[index]['type'] == 'NUMBER':
       answer = tokens[index]['number']
       return answer
 
+    # when factor is - factor
     elif tokens[index]['type'] == 'MINUS':
       answer = (-1) *  parse_factor(tokens[index + 1:])
       return answer
 
+    # when factor is function Parentheses
     elif tokens[index]['type'] == 'FUNCTION':
-      f_name = tokens[index]['name'].lower()
-      result = parse_parentheses(tokens[index+1:])
-      result_evaluate = eval(f"{f_name}({result})")
+      f_name = tokens[index]['name'].lower() # get function name
+      result = parse_parentheses(tokens[index+1:]) # parse Parentheses following function name
+
+      result_evaluate = eval(f"{f_name}({result})") # evaluate function P
       return result_evaluate
+
+    # when factor is Parentheses
     elif tokens[index]['type'] == 'PARENTHESES_LEFT':
       answer = parse_parentheses(tokens)
       return answer
@@ -115,22 +126,33 @@ def parse_factor(tokens):
         print('Invalid syntax')
         exit(1)
 
+# parse term to get the answer
+# |tokens| : a list of dictionary that represents each toekn
 def parse_term(tokens):
+
+  # store right parentheses and if the index is between (), skip it
   parentheses_right_stack = []
   answer = 0
   index = len(tokens) - 1
   while index >= 0:
+      # if the index is outside of (), if it is times, decompose to
+      # term and factor and multiply them
       if len(parentheses_right_stack) == 0:
         if tokens[index]['type'] == 'TIMES':
             factor1 = parse_term(tokens[:index])
             factor2 = parse_factor(tokens[index + 1:])
             answer = factor1 * factor2
             return answer
+
+        # if the index is outside of (), if it is divide, decompose to
+        # term and factor and divide them
         elif tokens[index]['type'] == 'DIVIDE':
             factor1 = parse_term(tokens[:index])
             factor2 = parse_factor(tokens[index + 1:])
             answer = factor1 / factor2
             return answer
+
+      # keep track of the number of parentheses
       if tokens[index]['type'] == 'PARENTHESES_RIGHT':
           parentheses_right_stack.append(index)
       elif tokens[index]['type'] == 'PARENTHESES_LEFT':
@@ -139,26 +161,40 @@ def parse_term(tokens):
             exit(1)
           parentheses_right_stack.pop()
       index -= 1
+
+  # When Term is Factor
   answer = parse_factor(tokens)
   return answer
 
+# parse expresssion to get the answer
+# |tokens| : a list of dictionary that represents each toekn
 def parse_expression(tokens):
   answer = 0
   index = len(tokens) - 1
 
+  # store right parentheses and if the index is between (), skip it
   parentheses_right_stack = []
   while index >= 0:
       if len(parentheses_right_stack) == 0:
+
+        # if the index is outside of (), if it is plus, decompose to
+        # expression and term and do addition.
         if tokens[index]['type'] == 'PLUS':
             term1 = parse_expression(tokens[:index])
             term2 = parse_term(tokens[index + 1:])
             answer = term1 + term2
             return answer
+
+        # skip the first minus
+        # if the index is outside of (), if it is minus, decompose to
+        # expression and term and do subtraction.
         elif index != 0 and tokens[index]['type'] == 'MINUS':
             term1 = parse_expression(tokens[:index])
             term2 = parse_term(tokens[index + 1:])
             answer = term1 - term2
             return answer
+
+      # keep track of the number of parentheses
       if tokens[index]['type'] == 'PARENTHESES_RIGHT':
           parentheses_right_stack.append(index)
       elif tokens[index]['type'] == 'PARENTHESES_LEFT':
@@ -168,11 +204,12 @@ def parse_expression(tokens):
           parentheses_right_stack.pop()
 
       index -= 1
+
+  # When Expression is Term
   answer = parse_term(tokens)
   return answer
 
 def evaluate(tokens):
-
   answer = parse_expression(tokens)
   return answer
 
